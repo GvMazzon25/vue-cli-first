04923f85c7c9c2937892df685f6642e593bb96db
GvMazzon25 <gvmazzon25@gmail.com>
2021-11-23 15:03:05 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
- 
+  <div id="app">
+    <Header />
+  </div>
 </template>
 
 <script>
+  import Header from '@/components/Header.vue';
 
+  export default{
+    
+  };
 </script>
 
-<style>
+<style lang='css'>
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+      <h1>HEADER HERE</h1>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
